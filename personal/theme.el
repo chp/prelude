2cdcc35322685009ecf0c89ab1d301a81abeb876
Chang Peng <chp@google.com>
2013-12-12 16:00:34 -0500

    custom theme.el

---
+++ personal/theme.el
@@ -0,0 +1,8 @@
+;; (prelude-require-package 'solarized-theme)
+;; (disable-theme 'zenburn)
+;; (load-theme 'solarized-dark t)
+
+(scroll-bar-mode -1)
+
+;; Enable arrow keys in chrome ssh sessions.
+(setq prelude-guru nil)
